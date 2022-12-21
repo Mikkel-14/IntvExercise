f9a7804f3ec420c6d96f73a7ceada608ce7b9442
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-12-21 00:17:39 -0500

    Update: main.py
    Functionality of the whole application integrated

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import sys
 from MainApp.StartApp import main
+from MainApp.EmployeeRoster import EmployeeRoster
+from MainApp.PairGenerator import EmployeePairGenerator
+from MainApp.OutPutter import StringOutPutter
 
-main(sys.argv)
+result = main(sys.argv)
+if isinstance(result, str):
+    print(result)
+else:
+    coincidences = EmployeeRoster(result).compute_coincident_employees(EmployeePairGenerator())
+    print(StringOutPutter().format(coincidences))
